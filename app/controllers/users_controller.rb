c192e472e61a36565c92c2a3d4266e6de5faf3c1
Chelsi Jones <cajones.bz@gmail.com>
2018-02-04 11:54:22 -0600

    Add Edit User form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update]
+
+	def index
+		@users = User.all
+	end
+
 	def new
+		@user = User.new
 	end
 
 	def create
-		@user = User.new(username: params["username"], email: params["email"], password: params["password"])
-		#@user = User.new(user_params)
+		#@user = User.new(username: params["username"], email: params["email"], password: params["password"])
+		@user = User.new(user_params)
 		if @user.save
 			redirect_to new_user_path
 		else
@@ -12,8 +19,27 @@ class UsersController < ApplicationController
 		end
 	end
 
+	#def show
+	#end
+
+	def edit
+	end
+
+	def update
+		# Unsure why field_with_errors wrapper is not displaying on page; investigate later?
+		if @user.update(user_params)
+			redirect_to edit_user_path
+		else
+			render :edit
+		end
+	end
+
 
 	private
+	def set_user
+		@user = User.find(params[:id])
+	end
+
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
 	end
